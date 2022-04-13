1696353dc2490173840ebae9a0fb5c5d01fe0783
The Changer <thechanger412@gmail.com>
2022-04-13 13:47:03 -0500

    made the mod appear in the game

---
+++ modinfo.lua
@@ -0,0 +1,17 @@
+name = "Terraria King Slime Boss"
+author = "The Changer"
+version = "1.0"
+description = "This is a mod that adds in the King Slime boss from Terraria."
+forumthread = ""
+
+icon_atlas = "icon.xml"
+icon = "icon.tex"
+
+api_version = 10
+
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+dst_compatible = true
+
+all_clients_require_mod = true
