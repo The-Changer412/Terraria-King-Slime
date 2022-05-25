d55c3b0fde444492d08f2904977bc33fa3ecf237
The Changer <thechanger412@gmail.com>
2022-05-25 11:03:58 -0500

    make the slime have the wander and idle state

--- scripts/brains/blue_slime_brain.lua
+++ scripts/brains/blue_slime_brain.lua
@@ -7,8 +7,10 @@ require "behaviours/standstill"
 --set the properties for the animation frame
 local ANIM_FRAME_MAX = 1
 local ANIM_FRAME_STATE = 0
-local SEE_DIST = 12
+local SEE_DIST = 9
 local ATTACK_DIST = 1
+local wander_time = 0
+local state_switch = math.random(1, 80)
 local state = "idle"
 local closest_dis = nil
 local closest_player = nil
@@ -25,11 +27,11 @@ function blue_slime_brain:OnStart()
     --no idea what this does, but its probably needed
     local root = PriorityNode(
     {
+        --switch state to dead when it died
         WhileNode(function()
             if self.inst.components.health.currenthealth <= 0 then
                 state = "dead"
             end
-            print(state)
         end),
 
         --function to manually animate and loop since it normal way of doing it didnt work for me
@@ -58,64 +60,119 @@ function blue_slime_brain:OnStart()
             end
         end),
 
-        -- make the slime see how far away the players are and decide what to do next
+        --make the slime see how far away the players are and decide what to do next
         WhileNode(function()
-            --set the dis
+            --set the distance
             local dis = nil
 
             --make it only get the distance when alive
             if state ~= "dead" then
-                -- get the slime pos
+                --get the slime pos
                 local x, y, z = self.inst.Transform:GetWorldPosition()
 
-                -- loop over all of the players to see which one is the closest
+                --loop over all of the players to see which one is the closest
                 for i, v in ipairs(AllPlayers) do
-                    -- get the player pos and the distance of them from the slime
+                    --get the player pos and the distance of them from the slime
                     local vx, vy, vz = v.Transform:GetWorldPosition()
                     dis = math.sqrt((vx - x)^2 + (vz - z)^2)
 
-                    -- if the player is the closest then reset the target
+                    --if the player is the closest then reset the target
                     if closest_dis == nil or closest_dis > dis then
                         closest_dis = dis
                         closest_player = v
                     end
                 end
 
+                --check the distance to see if it's in the seeing distance
                 if SEE_DIST >= dis then
+                    --check to see to chase them or to attack them
                     if ATTACK_DIST >= dis then
                         state = "attack"
                     else
                         state = "chase"
                     end
+                    wander_time = 0
                 else
-                    state = "wander"
-                    closest_player = nil
-                    self.inst.components.combat:SetTarget(nil)
+                    --randomly pick to wander or to idle
+                    if state ~= "wander" and state ~= "idle" then
+                        if math.random(0, 1) == 0 then
+                            state = "wander"
+                        else
+                            state = "idle"
+                        end
+                        --reset the targeting system
+                        closest_player = nil
+                        closest_dis = nil
+                        self.inst.components.combat:SetTarget(nil)
+                        self.inst.components.locomotor:Stop()
+
+                    --make it randomly switch between wander and idle after cooldown
+                    elseif state == "wander" or state == "idle" then
+                        if state_switch <= 0 then
+                            if math.random(0, 1) == 0 then
+                                state = "wander"
+                            else
+                                state = "idle"
+                            end
+                            state_switch = math.random(1, 80)
+                        else
+                            state_switch = state_switch - 1
+                        end
+                    end
                 end
             end
         end),
 
-        --chase after the target close enough
+        -- make the slime not move when idle
         WhileNode(function()
-            --get the combat
-            local combat = self.inst.components.combat
-
-            -- set the closest player as the target and chase them
-            if closest_player ~= nil then
-                --set the target
-                combat:SetTarget(closest_player)
-
-                --get the positon of the target and the angle
-                local target_position = Point(combat.target.Transform:GetWorldPosition())
-                local angle = self.inst:GetAngleToPoint(target_position)
-
-                --face the target and chase after them
-                self.inst.Transform:SetRotation(angle)
-                self.inst.components.locomotor:RunForward()
-            else
+            if state == "idle" then
+                -- stop any movement
+                wander_time = 0
                 closest_player = nil
-                combat:SetTarget(nil)
+                closest_dis = nil
+                self.inst.components.combat:SetTarget(nil)
                 self.inst.components.locomotor:Stop()
+            end
+        end),
+
+        -- make the slime wander around the world
+        WhileNode(function()
+            if state == "wander" then
+                if wander_time <= 0 then
+                    local angle = math.random(-180, 180)
+                    self.inst.Transform:SetRotation(angle)
+                    self.inst.components.locomotor:RunForward()
+                    wander_time = math.random(2, 12)
+                else
+                    wander_time = wander_time - 1
+                end
+            end
+        end),
+
+        --chase after the target
+        WhileNode(function()
+            if state == "chase" then
+                --get the combat
+                local combat = self.inst.components.combat
+
+                -- set the closest player as the target and chase them
+                if closest_player ~= nil then
+                    --set the target
+                    combat:SetTarget(closest_player)
+
+                    --get the positon of the target and the angle
+                    local target_position = Point(combat.target.Transform:GetWorldPosition())
+                    local angle = self.inst:GetAngleToPoint(target_position)
+
+                    --face the target and chase after them
+                    self.inst.Transform:SetRotation(angle)
+                    self.inst.components.locomotor:RunForward()
+                else
+                    closest_player = nil
+                    closest_dis = nil
+                    combat:SetTarget(nil)
+                    self.inst.components.locomotor:Stop()
+                end
             end
         end),
 
